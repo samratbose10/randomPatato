8ae5773d5d05da36a1281d81b108ed85f3f1a0b4
samratbose10 <samratofficial2009@gmail.com>
2024-06-27 07:38:46 +0530

    project completed

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const potatoImages = [
-        "alloq1.jpg",
+    const potatoImages = [
         "aloooo.jpg",
-        "pototoo.jpg",
+        "allo1.jpg",
+        "pototoo.png"
     ];
 
-  const potatoImg = document.getElementById("aloo");
-  potatoImg.src = potatoImages[Math.floor(Math.random() * potatoImages.length)];
+    const aloo = document.getElementById("aloo");
+    const nayaAlooButton = document.getElementById("nayaAloo");
+
+    const loadRandomPotato = () => {
+        aloo.classList.remove("fade-in");
+        aloo.classList.remove("rotate");
+        const randomImage = potatoImages[Math.floor(Math.random() * potatoImages.length)];
+        aloo.src = randomImage;
+        aloo.style.width = "200px";
+        aloo.onload = () => {
+            aloo.classList.add("fade-in");
+            aloo.classList.add("rotate");
+        };
+    };
+
+    loadRandomPotato();
+
+    aloo.addEventListener("click", () => {
+        const currentWidth = aloo.clientWidth;
+        aloo.style.width = currentWidth + 20 + "px";
+        aloo.classList.add("rotate");
+        setTimeout(() => {
+            aloo.classList.remove("rotate");
+        }, 1000);
+    });
+
+    nayaAlooButton.addEventListener("click", loadRandomPotato);
+});

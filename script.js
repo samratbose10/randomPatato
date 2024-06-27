2818222af5a87c7169233e90fc2da356f7012df3
samratbose10 <samratofficial2009@gmail.com>
2024-06-27 06:19:53 +0530

    basic structure

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const potatoImages = [
+        "alloq1.jpg",
+        "aloooo.jpg",
+        "pototoo.jpg",
+    ];
+
+  const potatoImg = document.getElementById("aloo");
+  potatoImg.src = potatoImages[Math.floor(Math.random() * potatoImages.length)];
+&& 
